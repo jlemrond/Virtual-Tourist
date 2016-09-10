55ce70bb0cc3cbf8f7fdb20bf2746b938068b90e
Jason Lemrond <jason.lemrond@icloud.com>
2016-09-10 16:34:56 -0400

    Add CoreDataStack.swift File

---
+++ CoreDataStack.swift
@@ -0,0 +1,182 @@
+//
+//  CoreDataStack.swift
+//  VirtualTourist
+//
+//  Created by Jason Lemrond on 9/10/16.
+//  Copyright © 2016 Jason Lemrond. All rights reserved.
+//
+
+import CoreData
+
+struct CoreDataStack {
+
+    // ******************************************************
+    //   MARK: - Variables
+    // ******************************************************
+
+    private let model: NSManagedObjectModel
+    private let storeCoordinator: NSPersistentStoreCoordinator
+
+    private let persistentContext: NSManagedObjectContext
+    private let backgroundContext: NSManagedObjectContext
+    let mainContext: NSManagedObjectContext
+
+    private let modelURL: NSURL
+    private let databaseURL: NSURL
+
+    typealias Batch = (context: NSManagedObjectContext) -> ()
+
+
+
+    // *************************************
+    //   MARK: - Initialize
+    // *************************************
+
+    init?(model modelName: String) {
+
+        // Get URL for model specified.
+        guard let modelURL = NSBundle.mainBundle().URLForResource(modelName, withExtension: "momd") else {
+            print("Unable to find data model named \(modelName)")
+            return nil
+        }
+
+        self.modelURL = modelURL
+
+
+        // Setup Model.
+        guard let model = NSManagedObjectModel(contentsOfURL: modelURL) else {
+            print("Unable to find model at \(modelURL)")
+            return nil
+        }
+
+        self.model = model
+
+
+        // Setup Store Coordinator.
+        storeCoordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+
+
+        // Setup Contexts.
+        persistentContext = NSManagedObjectContext(concurrencyType: .PrivateQueueConcurrencyType)
+        persistentContext.persistentStoreCoordinator = storeCoordinator
+        persistentContext.name = "Persistent Context"
+
+        mainContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        mainContext.parentContext = persistentContext
+        mainContext.name = "Main Context"
+
+        backgroundContext = NSManagedObjectContext(concurrencyType: .PrivateQueueConcurrencyType)
+        backgroundContext.parentContext = mainContext
+        backgroundContext.name = "Background Context"
+
+
+        // Get Database URL From File Manager
+        let fileManager = NSFileManager.defaultManager()
+
+        guard let documentsURL = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first else {
+            print("Unable to get database URL from File Manager Directory")
+            return nil
+        }
+
+        databaseURL = documentsURL.URLByAppendingPathComponent("model.sqlite")
+
+
+        // Create Persistent Store
+        do {
+            try storeCoordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: databaseURL, options: nil)
+        } catch {
+            print("Unable to add store at \(databaseURL)")
+        }
+
+    }
+
+
+
+    // ******************************************************
+    //   MARK: - Delete Data Functions
+    // ******************************************************
+
+    /// Drop all Data and restore with an empty data set.
+    func dropAllData() throws {
+
+        try storeCoordinator.destroyPersistentStoreAtURL(databaseURL, withType: NSSQLiteStoreType, options: nil)
+
+        try storeCoordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: databaseURL, options: nil)
+
+    }
+
+
+
+    // ******************************************************
+    //   MARK: - Save Data Functions
+    // ******************************************************
+
+    /// Save Data on specified context.
+    func saveContext(context: NSManagedObjectContext) {
+        do {
+            try context.save()
+        } catch {
+            fatalError("Error while saving \(context.name): \(error)")
+        }
+    }
+
+
+    /// Save Data from Main Thread via Persistent Context.
+    func save() {
+
+        mainContext.performBlock { 
+
+            if self.mainContext.hasChanges {
+
+                // Save from Main Context to Persistent Context.
+                self.saveContext(self.mainContext)
+
+                // Save form Persistent Context to Store Coordinator.
+                self.persistentContext.performBlock({ 
+                    self.saveContext(self.persistentContext)
+                })
+
+            }
+
+        }
+
+    }
+
+
+    /// Auto Save based on the Time interval specified.
+    func autoSave(delayInSeconds: Int) {
+
+        if delayInSeconds > 0 {
+            print("Autosaving.")
+
+            save()
+
+            performOnMainAfter(delayInSeconds, action: { 
+                self.autoSave(delayInSeconds)
+            })
+            
+        }
+
+    }
+
+
+
+    // ******************************************************
+    //   MARK: - Batch Data Functions
+    // ******************************************************
+
+    /// Create Batch on Background Thread
+    func performBackgroundBatchOperation(batch: Batch) {
+
+        backgroundContext.performBlock {
+            // Perform operation in closure.
+            batch(context: self.backgroundContext)
+
+            // Save to Main Context.
+            self.saveContext(self.backgroundContext)
+
+        }
+
+    }
+
+}
---
+++ Utilities.swift
@@ -0,0 +1,111 @@
+//
+//  Utilities.swift
+//  VirtualTourist
+//
+//  Created by Jason Lemrond on 9/10/16.
+//  Copyright © 2016 Jason Lemrond. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+func performOnMain(action: () -> Void) {
+
+    dispatch_async(GlobalQueue.main) {
+        action()
+    }
+
+}
+
+func performHighPriority(action action: () -> Void) {
+
+    dispatch_async(GlobalQueue.interactive) {
+        action()
+    }
+
+}
+
+func performStandardPriority(action action: () -> Void) {
+
+    dispatch_async(GlobalQueue.initiated) {
+        action()
+    }
+
+}
+
+func performInBackground(action: () -> Void) {
+
+    dispatch_async(GlobalQueue.utility) {
+        action()
+    }
+
+}
+
+func performLowPriority(action action: () -> Void) {
+
+    dispatch_async(GlobalQueue.utility) {
+        action()
+    }
+
+}
+
+func performOnMainAfter(seconds: Int, action: () -> Void) {
+
+    let delay = UInt64(seconds) * NSEC_PER_SEC
+    dispatch_after(delay, GlobalQueue.main) { 
+        action()
+    }
+
+}
+
+
+
+struct GlobalQueue {
+    static var main: dispatch_queue_t {
+        return dispatch_get_main_queue()
+    }
+
+    static var interactive: dispatch_queue_t {
+        return dispatch_get_global_queue(Int(QOS_CLASS_USER_INTERACTIVE.rawValue), 0)
+    }
+
+    static var initiated: dispatch_queue_t {
+        return dispatch_get_global_queue(Int(QOS_CLASS_USER_INITIATED.rawValue), 0)
+    }
+
+    static var utility: dispatch_queue_t {
+        return dispatch_get_global_queue(Int(QOS_CLASS_UTILITY.rawValue), 0)
+    }
+
+    static var background: dispatch_queue_t {
+        return dispatch_get_global_queue(Int(QOS_CLASS_BACKGROUND.rawValue), 0)
+    }
+
+    static var defaultQueue: dispatch_queue_t {
+        return dispatch_get_global_queue(Int(QOS_CLASS_DEFAULT.rawValue), 0)
+    }
+}
+
+
+
+// MARK: - Alert Controller Functions
+
+extension UIViewController {
+
+    func displayOneButtonAlert(title: String?, message: String?) {
+
+        if message != nil {
+            print("Error: \(message)")
+        }
+
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        performOnMain {
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+}
+
